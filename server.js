9e368930447bacc30f1c524956c059a7ebdb137d
Joseph Keepers <josephkeepers@gmail.com>
2023-10-18 14:58:57 -0400

    added server changes

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.use("/post", postRoutes);
 app.use("/comment", commentRoutes);
 
 //Server Running
-app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running on ${PORT}, you better catch it!`);
 });
